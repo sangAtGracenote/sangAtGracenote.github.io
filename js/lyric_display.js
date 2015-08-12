586ec8f6dcdade7133b2ea7ef24c794e4c5e9709
panavrin <panavrin@gmail.com>
2015-08-12 15:10:28 -0700

    support mp3

--- js/lyric_display.js
+++ js/lyric_display.js
@@ -59,7 +59,7 @@ function FileSelectHandler(e) {
     // process all File objects
     for (var i = 0, f; f = files[i]; i++) {
         ParseFile(f);
-        if (f.type == "audio/wav" || f.type == "audio/mpeg")
+        if (f.type == "audio/wav" || f.type == "audio/mpeg"|| f.type == "audio/mp3")
         {
             Output("<p>sound file is ready!</p>");
             $("#playwave").css("display", "inherit");
